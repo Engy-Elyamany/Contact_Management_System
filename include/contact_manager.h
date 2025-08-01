d8325ff38935a34020c16c51200400d1ea367949
saffanahatim <110130323+saffanahatim@users.noreply.github.com>
2025-08-01 13:10:40 +0200

    Update contact_manager.h

--- include/contact_manager.h
+++ include/contact_manager.h
@@ -37,6 +37,7 @@ node *search_contacts_by_phoneNum(node *head, char *phoneNum);
 
 // main business logic functions
 void sort_list_by_name( node *head, int count);
+int add_contact(node **head);
 void delete_contact(node **head);
 void search_contacts(node *head);
 void display_list(node *head); 
